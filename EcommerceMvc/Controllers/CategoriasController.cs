0ec6bb67f70c74aff10b2112835cc81d0202159e
Sheila Matos <sheilaanalumds01@gmail.com>
2023-02-21 18:23:02 -0300

    Primeira controller

---
+++ EcommerceMvc/Controllers/CategoriasController.cs
@@ -0,0 +1,18 @@
+﻿using EcommerceMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EcommerceMvc.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Categoria> list = new List<Categoria>();
+            list.Add(new Categoria { Id = 1, Nome = "Eletronico" });
+            list.Add(new Categoria { Id = 2, Nome = "Vestuário" });
+
+            return View(list);
+        }
+    }
+}
---
+++ EcommerceMvc/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace EcommerceMvc.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
