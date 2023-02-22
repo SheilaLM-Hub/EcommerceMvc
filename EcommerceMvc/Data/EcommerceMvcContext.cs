157684f4c6d4818b2c714d3cc9a59263ea1321a3
Sheila Matos <sheilaanalumds01@gmail.com>
2023-02-21 21:01:42 -0300

    CRUD Scaffolding

---
+++ EcommerceMvc/Data/EcommerceMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EcommerceMvc.Models;
+
+namespace EcommerceMvc.Data
+{
+    public class EcommerceMvcContext : DbContext
+    {
+        public EcommerceMvcContext (DbContextOptions<EcommerceMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EcommerceMvc.Models.Categoria> Categoria { get; set; }
+    }
+}
